22566610d9d080c9a573bec9ad11a97fe9899e4e
WastefulNick <31826825+WastefulNick@users.noreply.github.com>
2018-05-22 14:13:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ bot.on('message', message => {
     }
 
     if (msg.startsWith('TESTING')) {
-      message.channel.send(message.guild.channels.find);
+      message.guild.channels.find("name", "channel-name");
 
     }
 
