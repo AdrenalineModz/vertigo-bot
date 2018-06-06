ec96c3b267f1cdf122ef4c5c101503997445b4a9
WastefulNick <31826825+WastefulNick@users.noreply.github.com>
2018-06-06 21:37:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -90,8 +90,8 @@ bot.on('message', message => {
 
     if (msg.startsWith(prefix + 'GIVEAWAY')) {
       if(!message.member.roles.has("453883564417744907")) return message.channel.send("Command only for bot developers");
-      coins = message.content.slice(prefix.length + 2);
-      message.channel.send('GIVEAWAY\nI\'ll be drawing one winner to receive' + coins + 'Dank Memer coins! To participate in this giveaway all you have to do is be the Member rank or above and react to this message with :tada:!\n@453936238563557380\nGood luck!')
+      coins = message.content.slice(prefix.length + 8);
+      message.channel.send('**GIVEAWAY**\n\nI\'ll be drawing one winner to receive' + coins + ' Dank Memer coins! To participate in this giveaway all you have to do is be the Member rank or above and react to this message with :tada:!\n\n@453936238563557380\n\nGood luck!')
 
     }
 
