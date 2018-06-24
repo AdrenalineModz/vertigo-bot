b27f6bd639aed45c1f87929702eee17bbb55d9a1
WastefulNick <31826825+WastefulNick@users.noreply.github.com>
2018-06-24 18:34:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ bot.on('message', message => {
     }
 
     if(message.channel.id === '454020003885219840'){
-      message.member.addRole('454310213768708096');
+      message.member.addRole(454310213768708096);
       message.delete();
     }
 
