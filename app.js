97332888183630da1662a8c700ec948d2bf9cbd4
WastefulNick <31826825+WastefulNick@users.noreply.github.com>
2018-06-24 18:25:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -98,8 +98,8 @@ bot.on('message', message => {
     }
 
     if(message.channel === '454020003885219840'){
-      sender.addRole(454310213768708096)
-      message.delete()
+      sender.addRole(454310213768708096);
+      message.delete();
     }
 
     if (msg === prefix + 'PORN') {
